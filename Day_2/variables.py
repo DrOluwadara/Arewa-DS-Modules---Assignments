4463c6e1895673aa8c4fda0bb59c6ffa30d7c14b
DrOluwadara <lolaseundara@gmail.com>
2024-11-25 12:07:49 -0800

    Module 2 Exercises

---
+++ Day_2/variables.py
@@ -0,0 +1,83 @@
+# 'Day 2: 30 Days of python programming'
+
+# Exercises: Level 1
+first_name = "Omolola"
+last_name = "Oluwadara"
+full_name = "Omolola_Oluwadara"
+country = "Nigeria"
+city = "Lagos"
+age = 39
+year = 2024
+is_married = "yes"
+is_true = True
+is_light_on = "yes"
+a, b, c = (4, 8, 12)
+
+# Exercises: Level 2
+# 1
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+# 2
+length1 = len(first_name)
+print(length1)
+
+# 3
+length2 = len(last_name)
+print(length1, length2)
+
+# 4
+num_one = 5
+num_two = 4
+# 5
+total = num_one + num_two
+
+# 6
+diff = num_one - num_two
+
+# 7
+product = num_one * num_two
+
+# 8
+division = num_one / num_two
+
+# 9
+remainder = num_one % num_two
+
+# 10
+exp = num_one ** num_two
+
+# 11
+floor_division = num_one // num_two
+
+print(total, diff, product, division, remainder, exp, floor_division)
+
+#12
+r = 30 
+PI = 3.142
+area_of_circle = PI * r ** 2
+circum_of_circle = 2 * PI * r
+
+print(area_of_circle)
+print(circum_of_circle)
+
+R = input('What is the radius ')
+area = PI * int(R) ** 2
+print(area)
+
+# 13
+firstname = input('enter firstname ')
+lastname = input('enter lastname ')
+country_ = input('enter country ')
+age_ = input('enter age ')
+
+print(firstname, lastname, country_, age_)
+
